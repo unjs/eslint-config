c6b4489b4c27872174d142cac8b39bbbf130a805
Pooya Parsa <pooya@pi0.io>
2023-01-21 11:51:29 +0100

    fix: disable `indent` and `template-ident` formatting rules

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'prettier',
-    'standard',
-    'plugin:unicorn/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings'
+    "prettier",
+    "standard",
+    "plugin:unicorn/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    'import/resolver': {
-      node: { extensions: ['.js', '.mjs', '.cjs'] },
-      typescript: {}
-    }
+    "import/resolver": {
+      node: { extensions: [".js", ".mjs", ".cjs"] },
+      typescript: {},
+    },
   },
   rules: {
     // Disable formatting rules
     semi: 0,
     quotes: 0,
+    indent: 0,
     "space-before-function-paren": 0,
     "arrow-parens": 0,
     "comma-dangle": 0,
     "keyword-spacing": 0,
     "no-multiple-empty-lines": 0,
     "no-trailing-spaces": 0,
+    "unicorn/number-literal-case": 0,
+    "unicorn/template-indent": 0,
+    "generator-star-spacing": 0,
 
     // Disable some unnecessary or conflicting rules
     "no-use-before-define": "off",
@@ -41,45 +45,46 @@ module.exports = {
     "@typescript-eslint/no-empty-function": 0,
     "@typescript-eslint/no-var-requires": 0,
     "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/no-empty-interface": 0,
 
     // Enforce import order
-    'import/order': 'error',
+    "import/order": "error",
 
     // Imports should come first
-    'import/first': 'error',
+    "import/first": "error",
 
     // Other import rules
-    'import/no-mutable-exports': 'error',
+    "import/no-mutable-exports": "error",
 
     // Allow unresolved imports
-    'import/no-unresolved': 'off',
+    "import/no-unresolved": "off",
 
     // Prefer const over let
-    'prefer-const': [
-      'error',
+    "prefer-const": [
+      "error",
       {
-        destructuring: 'any',
-        ignoreReadBeforeAssign: false
-      }
+        destructuring: "any",
+        ignoreReadBeforeAssign: false,
+      },
     ],
 
     // No single if in an "else" block
-    'no-lonely-if': 'error',
+    "no-lonely-if": "error",
 
     // Force curly braces for control flow,
     // including if blocks with a single statement
-    curly: ['error', 'all'],
+    curly: ["error", "all"],
 
     // No async function without await
-    'require-await': 'error',
+    "require-await": "error",
 
     // Force dot notation when possible
-    'dot-notation': 'error',
+    "dot-notation": "error",
 
     // Force object shorthand where possible
-    'object-shorthand': 'error',
+    "object-shorthand": "error",
 
     // No useless destructuring/importing/exporting renames
-    'no-useless-rename': 'error'
-  }
-}
+    "no-useless-rename": "error",
+  },
+};
